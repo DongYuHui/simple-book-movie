6a1821ca9a683b5b80bb4c91e297b8b50701729b
DongYuHui <dyh920827@gmail.com>
2017-03-20 16:10:16 +0800

    修改主页与控件

--- app/src/main/java/com/kyletung/simplebookmovie/view/SwitchLayout.java
+++ app/src/main/java/com/kyletung/simplebookmovie/view/SwitchLayout.java
@@ -122,6 +122,12 @@ public class SwitchLayout extends ViewGroup {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if (!changed) return;
         int childCount = getChildCount();
