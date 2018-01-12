7aa5c0cb83297d8de33b9afe7242cdbc4c76c7f3
DongYuHui <dyh920827@gmail.com>
2018-01-12 17:00:37 +0800

    遗弃项目

--- commonlib/src/main/java/com/kyletung/commonlib/load/OnLoadMoreListener.java
+++
@@ -1,8 +0,0 @@
-package com.kyletung.commonlib.load;
-
-/**
- * Created by Aspsine on 2015/8/13.
- */
-public interface OnLoadMoreListener {
-    public void onLoadMore();
-}
--- commonlib/src/main/java/com/kyletung/commonlib/load/SwipeLoadMoreTrigger.java
+++
@@ -1,8 +0,0 @@
-package com.kyletung.commonlib.load;
-
-/**
- * Created by Aspsine on 2015/8/17.
- */
-public interface SwipeLoadMoreTrigger {
-    void onLoadMore();
-}
